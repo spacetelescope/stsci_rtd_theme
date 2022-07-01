9d207bb381f65ebaf9f14b9cd5b3856be0286408
zacharyburnett <zburnett@stsci.edu>
2022-07-01 12:38:10 -0400

    move build configuration into `pyproject.toml`

--- setup.py
+++ setup.py
@@ -1,34 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='stsci_rtd_theme',
-    version='1.0.0',
-    author='STScI',
-    author_email='help@stsci.edu',
-    description='STScI Branded Sphinx theme',
-    zip_safe=False,
-    packages=['stsci_rtd_theme'],
-    package_data={'stsci_rtd_theme': [
-                 'theme.conf',
-                 'static/*',
-                 '*.html'
-    ]},
-    include_package_data=True,
-    url='https://github.com/spacetelescope/stsci_rtd_theme/',
-    license='BSD',
-    entry_points={
-        'sphinx.html_themes': [
-            'stsci_rtd_theme = stsci_rtd_theme',
-        ],
-    },
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
-        'Topic :: Documentation',
-        'Topic :: Software Development :: Documentation',
-    ],
-    install_requires=('sphinx>=1.3', 'sphinx-rtd-theme'),
-)
+setup()
